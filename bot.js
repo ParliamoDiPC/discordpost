262c9cdcab2f9b8c4e4460a6457e86ab50e47e7d
ParliamoDiPC <66361695+ParliamoDiPC@users.noreply.github.com>
2021-05-19 22:42:04 +0200

    Writing the basic code

--- bot.js
+++ bot.js
@@ -1,1 +1,18 @@
+const Discord = require("discord.js")
+const BotConfiguration = require("bot_configuration.json")
 
+const bot = new Discord.Client()
+
+bot.on("ready", () => {
+    bot.application.commands.create({ name: "post", description: "Post something on " + BotConfiguration.site, options: [ { name: "title", description: "The post title", type: "STRING", required: true }, { name: "post_text", description: "The post text", type: "STRING", required: true } ] })
+    console.log("[i] Started DiscordPost bot")
+})
+
+bot.on("interaction", interaction => {
+    if (!interaction.isCommand()) return
+    if (interaction.commandName === "post") {
+        // WIP ;)
+    }
+})
+
+bot.login(BotConfiguration.bot_token)
